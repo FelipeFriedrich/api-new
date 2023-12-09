fb65ea4e35e9000e1f381a8b814ffbdf35b28430
Felipe Friedrich Santos <54379678+FelipeFriedrich@users.noreply.github.com>
2023-12-08 23:59:56 -0300

    feat: rentBook

---
+++ src/controllers/rentBook.js
@@ -0,0 +1,83 @@
+const { request } = require('express');
+const RentBookDataBase = require('../database/models/rentBook');
+
+module.exports = class Book {
+
+  static all(req, res, next) {
+    RentBookDataBase.findAll({
+      raw: true,
+      attributes: ['id', 'status', 'dataRetirada', 'dataDevolucao'],
+      include: [
+        {
+          model: BooksDataBase,
+          as: 'book',
+          attributes: ['id', 'nome']
+        },
+        {
+          model: CostumersDataBase,
+          as: 'costumer',
+          attributes: ['id', 'nome']
+        }
+      ]
+    })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch(next);
+  }
+
+  static findByPK(req, res, next) {
+    try {
+      const { id } = req.params;
+      RentBookDataBase.findByPk(id).then((result) => {
+        res.json(result);
+      })
+        .catch(next);
+    }catch(err){
+      console.error(err);
+      res.status(404).json(err);
+    }
+  }
+
+  static create(req, res, next) {
+    const { idBook, idCostumer } = req.body;
+    const status = 'alugado';
+    const dataRetirada = new Date();
+    RentBookDataBase.create({
+      idBook,
+      idCostumer,
+      status,
+      dataRetirada
+    })
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch(next);
+  }
+
+  static async update(req, res, next) {
+    try {
+      const { id } = req.params;
+      const status = 'devolvido';
+      const dataDevolucao = new Date();
+      const response = await RentBookDataBase.update(
+        { status, dataDevolucao},
+        { where: { 'id': id } });
+        res.status(200).json(response);
+    } catch (err) {
+      return err;
+    }
+
+  }
+
+  static async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+      const response = await RentBookDataBase.destroy(
+        { where: { 'id': id } });
+        res.status(202).json(response);
+    } catch (err) {
+      return err;
+    }
+  }
+}
---
+++ src/database/models/rentBook.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../index");
+
+const RentBook = sequelize.define("rentBook", { 
+    id: {
+      type: DataTypes.INTEGER ,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,        
+    },
+    idBook: {
+      type: DataTypes.INTEGER,
+      references: {model: "Book", key: "id"},
+      allowNull: false,
+    },
+    idCostumer: {
+      type: DataTypes.INTEGER,
+      references: {model: "costumer", key: "id"},
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    dataRetirada: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    dataDevolucao: {
+      type: DataTypes.DATE,
+    },
+  });
+
+//create table if not exists...
+const init = async () => {
+  await RentBook.sync();
+};
+
+init();
+
+module.exports = RentBook;
