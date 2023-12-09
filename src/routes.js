e716dbfa53ee4f1e98c6a16b44897c9ea8552d67
Felipe Friedrich Santos <54379678+FelipeFriedrich@users.noreply.github.com>
2023-12-09 00:07:35 -0300

    fix: routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const  routes  = require('express').Router();
 const Books = require('./controllers/book');
 const Costumers = require('./controllers/costumer');
+const RentBook = require('./controllers/rentBook');
 
 routes.get('/', (request, response) => { return response.json('Funcionando') });
 
@@ -18,12 +19,12 @@ routes.post ('/book', Books.create);
 routes.patch ('/book/:id', Books.update);
 routes.delete ('/book/:id', Books.delete);
 
-/*
-routes.get ('/rentBook/:id', books.findByPK);
-routes.get ('/rentBooks', books.all);
-routes.post ('/rentBook', books.create);
-routes.patch ('/rentBook/:id', books.update);
-routes.delete ('/rentBook/:id', books.delete);
-*/
+
+routes.get ('/rentBook/:id', RentBook.findByPK);
+routes.get ('/rentBooks', RentBook.all);
+routes.post ('/rentBook', RentBook.create);
+routes.patch ('/rentBook/:id', RentBook.update);
+routes.delete ('/rentBook/:id', RentBook.delete);
+
 
 module.exports = routes;
